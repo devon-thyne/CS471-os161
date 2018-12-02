5b8afa5c1aedddef03ba8233f3c2380b50653997
Matthew D Gayhart <mgayhart@zeus-1.vse.gmu.edu>
2018-12-02 17:33:58 -0500

    Implemented getpid.c

--- os161-1.11/kern/userprog/getpid.c
+++ os161-1.11/kern/userprog/getpid.c
@@ -7,4 +7,20 @@
  * CS 471 - 001
  */
 
+#include <kern/types.h>
+#include <syscall.h>
+#include <thread.h>
+#include <curthread.h>
+
+/*
+ * Get the pid of the current process, value stored in the thread Struct
+ * returns 0 as get_pid does not fail
+ */
+
+int
+sys_getpid(pid_t *return_pid)
+{
+	*return_pid = curthread->pid;
+	return 0;
+}
 
