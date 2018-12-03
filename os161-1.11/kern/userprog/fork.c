d41b461e83ffa58ef407da838e6a4fecc13b8a5f
Matthew D Gayhart <mgayhart@zeus-1.vse.gmu.edu>
2018-12-03 18:29:58 -0500

    Began work creating fork.c skeleton

--- os161-1.11/kern/userprog/fork.c
+++ os161-1.11/kern/userprog/fork.c
@@ -7,4 +7,40 @@
  * CS 471 - 001
  */
 
+#include <types.h>
+#include <kern/errno.h>
+#include <thread.h>
+#include <curthread.h>
+#include <syscall.h>
+#include <machine/trapframe.h>
+
+
+
+/*
+ * Fork System call
+ * The child process returns 0, the Parent returns pid of child
+ *
+ * On Error, do not create child, return -1
+ *
+ */
+
+pid_t
+sys_fork(void)
+{
+	/*Create a PID for child process and add to process_table*/
+	
+	//child = get_new_pid()
+	//if(child == NULL) /can't get a pid
+	//	return -1
+	//else
+	//
+	//Child trapframe = parent Trapframe
+	//caddrspc = as_copy();
+	//thread_fork();
+	//
+	//??
+	//if child return 0
+	//if parent return child
+	return 0;
+}
 
