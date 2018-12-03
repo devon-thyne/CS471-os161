60778f10cd8f2659985178ef7fd4b7b8ed053cc7
Matthew D Gayhart <mgayhart@zeus-2.vse.gmu.edu>
2018-12-02 19:16:46 -0500

    Test for getpid() created

---
+++ os161-1.11/testbin/getpidtest/getpidtest.c
@@ -0,0 +1,23 @@
+/*
+ * forktest - test fork().
+ *
+ * This should work correctly when fork is implemented.
+ *
+ * It should also continue to work after subsequent assignments, most
+ * notably after implementing the virtual memory system.
+ */
+
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <err.h>
+
+int
+main(int argc, char *argv[])
+{
+	int pidcheck;
+
+	pidcheck = getpid();
+	return 0;
+}
